d0cf424a2795d80b823979f42df269a164631a2a
Shrikanttathe-hub <142688445+Shrikanttathe-hub@users.noreply.github.com>
2024-02-12 22:02:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,14 +19,9 @@ SportsCar.prototype.constructor = SportsCar;
 	return this.topspeed;
 };
 // Do not change the code below
-const SportsCar = win.SportsCar;
-  const car = new SportsCar("Ferrari", "Testarossa", 200);
-  console.log("Car Object:", car); // Check the car object
-  console.log("Top Speed:", car.getTopSpeed());
-
-// const car = new SportsCar("Ferrari", "Testarossa", 200);
-// console.log(car.getMakeModel()); // Output: Ferrari Testarossa
-// console.log(car.getTopSpeed()); // Output: 200
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+console.log(car.getMakeModel()); // Output: Ferrari Testarossa
+console.log(car.getTopSpeed()); 
 
 window.Car = Car;
 window.SportsCar = SportsCar;
